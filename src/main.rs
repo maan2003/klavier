f46bd145969901213540a8f3e935031f4f2fbd6e
Maan2003 <manmeetmann2003@gmail.com>
2021-11-29 01:07:07 +0530

    my layout: Remove some useless keys

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,45 @@
 #![feature(decl_macro)]
 
-mod rules;
 mod ext;
 mod min_keys;
+mod rules;
 
 use std::{collections::VecDeque, error::Error};
 mod all_keys;
 use evdev::Device;
-use rules::{keys, if_held, magic_shift, mod_or_key, remap, Rule};
 use min_keys::*;
-
+use rules::{if_held, keys, magic_shift, mod_or_key, remap, Rule};
 
 const DEVICE_PATH: &str = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
 
 fn rules() -> Vec<Box<dyn Rule>> {
     let src = keys!(
-        ESC    F1   F2   F3   F4   F5   F6   F7   F8   F9   F10   F11   F12
-        GRAVE   N1    N2    N3    N4    N5    N6    N7    N8    N9    N0         MINUS   EQUAL    BS
-        TAB     Q     W     E     R     T     Y     U     I     O     P          LBRACE  RBRACE   BACKSLASH
-        CAPS    A     S     D     F     G     H     J     K     L     SEMICOLON  APOSTROPHE       ENTER
-        LSHIFT  Z     X     C     V     B     N     M     COMMA DOT   SLASH           RSHIFT
+        TAB     Q     W     E     R     T     Y     U     I     O     P      LBRACE  RBRACE
+        CAPS    A     S     D     F     G     H     J     K     L     SMCLN  SQUOTE
+        LSHFT   Z     X     C     V     B     N     M     COMMA DOT   SLASH  RSHFT
         LCTRL   LMETA LALT            SPACE               RALT  RCTRL
     );
     // the colemak dh layout
     let cmk = keys!(
-        ESC    F1   F2   F3   F4   F5   F6   F7   F8   F9   F10   F11   F12
-        GRAVE   N1    N2    N3    N4    N5    N6    N7    N8    N9    N0         MINUS   EQUAL    BS
-        TAB     Q     W     F     P     B     J     L     U     Y     SEMICOLON  LBRACE  RBRACE   BACKSLASH
-        CAPS    A     R     S     T     G     M     N     E     I     O          APOSTROPHE       ENTER
-        LSHIFT  X     C     D     V     Z     K     H     COMMA DOT   SLASH           RSHIFT
+        TAB     Q     W     F     P     B     J     L     U     Y     SMCLN  LBRACE  RBRACE
+        CAPS    A     R     S     T     G     M     N     E     I     O      SQUOTE
+        LSHFT   X     C     D     V     Z     K     H     COMMA DOT   SLASH  RSHFT
         LCTRL   LMETA LALT            SPACE               RALT  RCTRL
     );
 
     let ext = remap!(
-        O => BS,
         N => LEFT,
         U => UP,
         E => DOWN,
         I => RIGHT,
+        O => BS,
     );
 
     vec![
         remap(&src, &cmk),
         magic_shift(),
         mod_or_key(CAPS, CAPS, F9),
+        mod_or_key(RALT, RALT, ENTER),
         if_held(CAPS, ext),
     ]
 }
