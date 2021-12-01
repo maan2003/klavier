ee93f6b077229c4cdc34857e039c5fabf8e71af1
Maan2003 <manmeetmann2003@gmail.com>
2021-12-01 18:01:51 +0530

    Magic shift: unit tests

--- src/rules/magic_shift.rs
+++ src/rules/magic_shift.rs
@@ -92,3 +92,113 @@ pub fn magic_shift() -> Box<dyn Rule> {
         state: ShiftState::None,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_util::*;
+    use crate::keys::*;
+
+    test_case! {
+        other_keys_fail_thru
+        rule = magic_shift(),
+        input = (
+            down C
+            down D
+            hold C
+            up C
+            up D
+        ),
+        output = (
+            down C
+            down D
+            hold C
+            up C
+            up D
+        ),
+    }
+
+    test_case! {
+        normal_shift_use
+        rule = magic_shift(),
+        input = (
+            down LSHT
+            down A
+            hold A
+            up A
+            up LSHT
+        ),
+        output = (
+            down LSHT
+            down A
+            hold A
+            up A
+            up LSHT
+        ),
+    }
+
+    test_case!(
+        single_tap
+        rule = magic_shift(),
+        input = (
+            down LSHT
+            up LSHT
+            down A
+            hold A
+            up A
+            down A
+            hold A
+            up A
+        ),
+        output = (
+            down LSHT
+            down A
+            hold A
+            up A
+            up LSHT
+            down A
+            hold A
+            up A
+        ),
+    );
+
+    test_case!(
+        double_tap
+        rule = magic_shift(),
+        input = (
+            down LSHT
+            up LSHT
+            down LSHT
+            up LSHT
+            down A
+            hold A
+            up A
+            down B
+            hold B
+            up B
+            down LSHT
+            up LSHT
+            // now release shift
+            down A
+            hold A
+            up A
+        ),
+        output = (
+            down LSHT
+            // TODO: find a way to not send this
+            down LSHT 
+            down A
+            hold A
+            up A
+            down B
+            hold B
+            up B
+            // TODO: find a way to not send this
+            down LSHT 
+            up LSHT
+            down A
+            hold A
+            up A
+        ),
+    );
+}
